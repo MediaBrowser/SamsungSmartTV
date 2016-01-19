3174a9a1a8347e4a6ddf52cd327a6d9b91d4d5f5
cmcg <colin@mcgerty.co.uk>
2016-01-19 13:03:01 +0000

    1.0.5
    Media icons limited to six.
    New media icons for 3D Side-by-Side and 3D Top-and-Bottom.
    Correct 16:9 media icon now used.
    Fixed: 3D media icon displayed twice.
    Episode details page now shows correct season poster.

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -4,7 +4,7 @@ var tvKey = new Common.API.TVKeyValue();
 	
 var Main =
 {
-		version : "v1.0.4",
+		version : "v1.0.5",
 		requiredServerVersion : "3.0.5211",
 		requiredDevServerVersion : "3.0.5507.2131",
 		
