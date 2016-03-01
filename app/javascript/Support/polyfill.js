4a63131fa7e78a675fa1fec00ed0b8f49a78962d
Andrew Schurman <arcticwaters@gmail.com>
2016-03-01 15:46:54 -0800

    Move Main.js over to webapis

--- app/javascript/Support/polyfill.js
+++ app/javascript/Support/polyfill.js
@@ -1,3 +1,18 @@
+if (!Array.prototype.find) {
+	Array.prototype.find = function(predicate, thisArg) {
+		if (typeof predicate !== 'function') {
+			throw new TypeError('predicate must be a function');
+		}
+
+		var lastVal;
+		if (Array.prototype.some.call(this, function(val, index, arr) {
+			return predicate.call(thisArg, lastVal = val, index, arr);
+		})) {
+			return lastVal;
+		}
+	}
+}
+
 if (!Array.prototype.findIndex) {
 	Array.prototype.findIndex = function(predicate, thisArg) {
 		if (typeof predicate !== 'function') {
