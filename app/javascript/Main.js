c0d94c0d5e559b815693fd7c7899be3d342469e8
Mark Barbieri <barbierimc@yahoo.com.au>
2019-03-07 22:23:30 +1100

    1.0.8

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -4,7 +4,7 @@ var tvKey = new Common.API.TVKeyValue();
 	
 var Main =
 {
-		version : "v1.0.7",
+		version : "v1.0.8",
 		requiredServerVersion : "3.0.5211",
 		requiredDevServerVersion : "3.0.5507.2131",
 		
